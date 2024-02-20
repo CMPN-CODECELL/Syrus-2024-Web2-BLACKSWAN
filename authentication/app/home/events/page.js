55549e196a60c277797897159fdbf6a242c35e73
Veda19g <vedang19gambhire@gmail.com>
2024-02-20 21:28:25 +0530

    profile page and event pages successfully implemented

---
+++ authentication/app/home/events/page.js
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Events() {
+    return (
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.4234436236798!2d72.88460313488768!3d19.0451113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c8add9569a29%3A0xb7ad04bf9a389df7!2sVivekanand%20Education%20Society&#39;s%20Institute%20Of%20Technology%20(VESIT)!5e0!3m2!1sen!2sin!4v1708418461691!5m2!1sen!2sin" width="300" height="250" classname="b-0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    )
+}
