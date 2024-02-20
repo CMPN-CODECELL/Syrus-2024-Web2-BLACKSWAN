c15a48ec4d08d6d51c83655fc612840f9fe37985
pankaj ram pawar <pankajpawars123@gmail.com>
2024-02-20 08:33:18 +0530

    user model made

---
+++ server/models/User.model.js
@@ -0,0 +1,55 @@
+const mongoose=require("mongoose");
+
+const bcrypt=require("bcrypt");
+
+const UserSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    name:{
+        type:String,
+    },
+    bio:{
+        type:String,   
+    },
+    profilePic:{
+        type:String,
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength: 6
+    },
+    bookmarks:[{
+        type:[mongoose.Schema.Types.ObjectId],
+        ref:"Post"
+    }],
+    points:{
+        type:Number,
+    },
+    ranking:{
+        type:Number,
+    },
+
+});
+
+UserSchema.pre("save", async function(next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+const User=new mongoose.model("User",UserSchema);
+
+module.exports=User;
